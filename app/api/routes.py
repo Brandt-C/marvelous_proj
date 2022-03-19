8ba400c9198d4d93d9323fc588f0ab4bf7c66a92
Brandt-C <brandt.carlson11@gmail.com>
2022-03-19 13:31:19 -0500

    api working- going to try to add gui

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, render_template, request, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
@@ -33,7 +33,35 @@ def getChars():
     chars = {'Char': [c.to_dict() for c in Char.query.all()]}
     return jsonify(chars), 200
 
+@api.route('/character/<string:s_name>')
+def getChar(s_name):
+    c = Char.query.filter_by(s_name=s_name.title()).first()
+    if c:
+        return jsonify(c.to_dict()), 200
+    else:
+        return jsonify({'No dice':'No character found by that entry.'}), 418
 
+@api.route('/character/update/<string:s_name>', methods=['PUT'])
+def updateChar(s_name):
+    try:
+        char = Char.query.get(s_name)
+        data = request.get_json()
+        char.from_dict(data)
+        db.session.commit()
+        return jsonify({'Character updated': char.to_dict()})
+    except:
+        return jsonify({'No dice.': 'maybe it was you, maybe it was us but that didn\'t work.'})
 
+@api.route('/character/del/<string:s_name>', methods=['DELETE'])
+def delChar(s_name):
+    char = Char.query.get(s_name)
+    if not char:
+        return jsonify({'Can\'t delete-': 'Not found or doesn\'t exist'}), 418
+    db.session.delete(char)
+    db.session.commit()
+    return jsonify({'I hope you\'re happy. . . Deleted-': char.to_dict()}), 200
 
+# @api.route('/add_char', methods=['GET', 'POST'])
+# def guiAddChar():
+#     return render_template('/add_char.html')
 
